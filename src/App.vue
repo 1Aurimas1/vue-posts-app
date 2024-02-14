7ae17d696d541dcd13628f6837eb4652b10656e0
1Aurimas1 <46860627+1Aurimas1@users.noreply.github.com>
2024-02-14 09:42:53 +0200

    Resolve "Modal"

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,9 @@
   <div id="app">
     <NavBar />
     <NotificationList />
+    <Modal>
+      <component :is="modal.component">Form loading error!</component>
+    </Modal>
     <router-view class="mt-5"></router-view>
   </div>
 </template>
@@ -11,6 +14,8 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import NavBar from "./components/NavBar.vue";
 import NotificationList from "./components/Notification/NotificationList.vue";
+import { mapGetters } from "vuex";
+import Modal from "./components/Modal.vue";
 
 Vue.use(VueRouter);
 
@@ -18,6 +23,10 @@ export default {
   components: {
     NavBar,
     NotificationList,
+    Modal,
+  },
+  computed: {
+    ...mapGetters(["modal"]),
   },
 };
 </script>
