5b4a113912fa03026dcef307ec0edf8d98a1b3d6
1Aurimas1 <46860627+1Aurimas1@users.noreply.github.com>
2024-02-23 10:10:08 +0200

    Resolve "Delete post"

---
+++ src/components/Forms/DeletePost.vue
@@ -0,0 +1,43 @@
+<template>
+  <div v-if="post">
+    <div class="field">
+      <label class="label">Title</label>
+      <div>{{ post.title }}</div>
+    </div>
+    <div class="field">
+      <label class="label">Author</label>
+      <div>{{ post.author.name }}</div>
+    </div>
+    <div class="field">
+      <label class="label">Content</label>
+      <div>{{ post.body }}</div>
+    </div>
+
+    <div class="control">
+      <button @click="handleDelete" class="button is-danger is-outlined">
+        Delete
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from "vuex";
+
+export default {
+  computed: {
+    ...mapGetters({ post: "singlePost" }),
+  },
+  methods: {
+    ...mapActions(["deletePost", "hideModal"]),
+    handleDelete() {
+      this.performCleanup();
+
+      this.deletePost(this.post.id);
+    },
+    performCleanup() {
+      this.hideModal();
+    },
+  },
+};
+</script>
